c79ec3735924bc99442e3d8a094a843a0c29abf9
Bonilapa <bonilapka@gmail.com>
2020-04-24 12:46:37 +0300

    application init. jpa + mysql connection

---
+++ src/main/java/com/example/SBA/Application.java
@@ -0,0 +1,18 @@
+/*
+    studied with
+    https://www.youtube.com/watch?v=nyFLX3q3poY&list=PLU2ftbIeotGpAYRP9Iv2KLIwK36-o_qYk&index=4
+ */
+
+
+package com.example.SBA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
